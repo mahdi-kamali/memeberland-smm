09812a7bfc84dae47df25e26acf344bf591b437e
wotox2 <49289130+wotox2@users.noreply.github.com>
2023-12-10 15:08:08 +0330

    Main page Completed

--- src/components/primaries/footer/Footer.tsx
+++ src/components/primaries/footer/Footer.tsx
@@ -1,9 +1,108 @@
+import Iconify from '@/components/icon/Iconify'
+import Image from 'next/image'
 import React from 'react'
 
 export default function Footer() {
     return (
         <footer>
-            footer
+
+            
+            <div className="header">
+                <div className="right">
+                    <Image
+                        src="/images/main-page/39.png"
+                        fill
+                        alt='' />
+                </div>
+                <h2 className="left">
+                    ممبرلند: همیشه با شما، راهی ساده به سوی رشد در فضای اجتماعی.
+                </h2>
+            </div>
+            <div className="social-links">
+                <div className="item">
+                    <Iconify
+                        icon="teenyicons:instagram-solid" />
+                    <span>
+                        www.member-land.com
+                    </span>
+                </div>
+                <div className="item">
+                    <Iconify
+                        icon="teenyicons:instagram-solid" />
+                    <span>
+                        www.member-land.com
+                    </span>
+                </div>
+                <div className="item">
+                    <Iconify
+                        icon="teenyicons:instagram-solid" />
+                    <span>
+                        www.member-land.com
+                    </span>
+                </div>
+            </div>
+            <div className="lists">
+                <ul>
+                    <h1>
+                        پشتیبانی
+                    </h1>
+
+                    <li>
+                        مستندات API
+                    </li>
+                    <li>
+                        تیکت ها
+                    </li>
+                    <li>
+                        شرایط و حقوق کاربران
+                    </li>
+                    <li>
+                        حریم خصوصی
+                    </li>
+                </ul>
+                <ul>
+                    <h1>
+                        پشتیبانی
+                    </h1>
+
+                    <li>
+                        مستندات API
+                    </li>
+                    <li>
+                        تیکت ها
+                    </li>
+                    <li>
+                        شرایط و حقوق کاربران
+                    </li>
+                    <li>
+                        حریم خصوصی
+                    </li>
+                </ul>
+                <ul>
+                    <h1>
+                        پشتیبانی
+                    </h1>
+
+                    <li>
+                        مستندات API
+                    </li>
+                    <li>
+                        تیکت ها
+                    </li>
+                    <li>
+                        شرایط و حقوق کاربران
+                    </li>
+                    <li>
+                        حریم خصوصی
+                    </li>
+                </ul>
+            </div>
+            <div className="rocket">
+                <Image
+                    src="/images/main-page/40.png"
+                    alt=''
+                    fill />
+            </div>
         </footer>
     )
 }
